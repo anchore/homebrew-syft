5d929f1709c2c9e3204e6c966077f61a8fe296ab
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-25 14:26:16 -0700

    Brew formula update for syft version v0.1.0-beta.5

--- syft.rb
+++ syft.rb
@@ -2,16 +2,16 @@
 class Syft < Formula
   desc "A tool that generates a Software Bill Of Materials (SBOM) from container images and filesystems"
   homepage "https://github.com/anchore/syft"
-  version "0.1.0-beta.4"
+  version "0.1.0-beta.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/anchore/syft/releases/download/v0.1.0-beta.4/syft_0.1.0-beta.4_darwin_amd64.tar.gz"
-    sha256 "6ea4af01209b3ee524561584065758ce489e572817c716e51f3b2ec0aa89a82b"
+    url "https://github.com/anchore/syft/releases/download/v0.1.0-beta.5/syft_0.1.0-beta.5_darwin_amd64.tar.gz"
+    sha256 "e786b6375475fe3b614aa76f4d53a4f23be3fb9ca8fe2fe4b76bd93b9f135ab2"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/anchore/syft/releases/download/v0.1.0-beta.4/syft_0.1.0-beta.4_linux_amd64.tar.gz"
-      sha256 "557107f0cf9f569c2588aa921cc41d54664e7c1ad12cd5d85e49393816b7c9b4"
+      url "https://github.com/anchore/syft/releases/download/v0.1.0-beta.5/syft_0.1.0-beta.5_linux_amd64.tar.gz"
+      sha256 "4e3592c9fd30b5a0d0b6a0e1e1b7e70bb5e92d0add59752bf8d46adfe1cef69e"
     end
   end
 

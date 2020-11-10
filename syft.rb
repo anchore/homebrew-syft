40efeb271422aac07c78828b469923a66f0ecc06
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-10 05:18:29 -0800

    Brew formula update for syft version v0.6.0

--- syft.rb
+++ syft.rb
@@ -2,14 +2,14 @@
 class Syft < Formula
   desc "A tool that generates a Software Bill Of Materials (SBOM) from container images and filesystems"
   homepage "https://github.com/anchore/syft"
-  version "0.5.1"
+  version "0.6.0"
   bottle :unneeded
 
   if OS.mac?
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/anchore/syft/releases/download/v0.5.1/syft_0.5.1_linux_amd64.tar.gz"
-      sha256 "185593adc167e4c23b9d7b40bbeb77985581dc08f6800af7ad9e6ce99be2979d"
+      url "https://github.com/anchore/syft/releases/download/v0.6.0/syft_0.6.0_linux_amd64.tar.gz"
+      sha256 "da352884fd1758936042991fb9417a1cab8115a53d99bdc38fdb01661a5b99db"
     end
   end
 

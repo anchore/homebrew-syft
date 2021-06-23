06b7c3a323afac99dfcae60e07bfd1143db90dba
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-23 14:45:17 -0700

    Brew formula update for syft version v0.17.1

--- syft.rb
+++ syft.rb
@@ -5,16 +5,16 @@
 class Syft < Formula
   desc "A tool that generates a Software Bill Of Materials (SBOM) from container images and filesystems"
   homepage "https://github.com/anchore/syft"
-  version "0.17.0"
+  version "0.17.1"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/anchore/syft/releases/download/v0.17.0/syft_0.17.0_darwin_amd64.zip"
-    sha256 "c017efbcfa6fc8157bc1ef2f08fe77ac0658e66d0c1a8389bc093edd05a2a287"
+    url "https://github.com/anchore/syft/releases/download/v0.17.1/syft_0.17.1_darwin_amd64.zip"
+    sha256 "95b2f6bf2bd1742387c139e7427491057cb8715b33d051bc57660c02eafd4c9e"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/anchore/syft/releases/download/v0.17.0/syft_0.17.0_linux_amd64.tar.gz"
-    sha256 "05f363e33a49d113fbd01ec55b56b9e7a4d92216ba621cdfd7d5055d9a4d9d99"
+    url "https://github.com/anchore/syft/releases/download/v0.17.1/syft_0.17.1_linux_amd64.tar.gz"
+    sha256 "c0a86174881c70fe7fbc049e8b01a70e2f660d8b21c0139166e5605cba41e095"
   end
 
   def install

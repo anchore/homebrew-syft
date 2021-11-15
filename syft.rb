57a285ab16371ffbc6a44f4113410e2eed33c3fd
goreleaserbot <goreleaser@carlosbecker.com>
2021-11-15 11:07:25 -0800

    Brew formula update for syft version v0.30.0

--- syft.rb
+++ syft.rb
@@ -5,27 +5,28 @@
 class Syft < Formula
   desc "A tool that generates a Software Bill Of Materials (SBOM) from container images and filesystems"
   homepage "https://github.com/anchore/syft"
-  version "0.29.0"
+  version "0.30.0"
+  bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/anchore/syft/releases/download/v0.29.0/syft_0.29.0_darwin_amd64.zip"
-      sha256 "9d3d62c0ef21e166e65fe749394f4734bc3606a88f3649504f4f95a59ce8a5a0"
+      url "https://github.com/anchore/syft/releases/download/v0.30.0/syft_0.30.0_darwin_amd64.zip"
+      sha256 "b3e217fe9145cd3fd7512b65b05faeede0a5c6b2f974325b07fdbec2ff757526"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/anchore/syft/releases/download/v0.29.0/syft_0.29.0_darwin_arm64.zip"
-      sha256 "85a8134b6f053648f5baa66dfbbb719c54e931d4e35032613ade45c98f71ae3e"
+      url "https://github.com/anchore/syft/releases/download/v0.30.0/syft_0.30.0_darwin_arm64.zip"
+      sha256 "20eb2554f7c168447d1d36ff16a26634ee7c2a8bd59ad0fe5b6e07b2967f948d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/anchore/syft/releases/download/v0.29.0/syft_0.29.0_linux_amd64.tar.gz"
-      sha256 "74a9becd5ccb4c02de99050a632e7bfc72176d5e4e844093b1e2c5bedfb45888"
+      url "https://github.com/anchore/syft/releases/download/v0.30.0/syft_0.30.0_linux_amd64.tar.gz"
+      sha256 "33501fe7e959a7a42f1e67ca87fc1ae6e70cfa00bd5c98e224313ad1d798e9f0"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/anchore/syft/releases/download/v0.29.0/syft_0.29.0_linux_arm64.tar.gz"
-      sha256 "9b6d77ef10a2aaf863820f2cf8a67cf2a3b158bd7f27c545fe3fcb7625bd79cc"
+      url "https://github.com/anchore/syft/releases/download/v0.30.0/syft_0.30.0_linux_arm64.tar.gz"
+      sha256 "a1b4912304e9b8214961c718d5c9e4b24e4b2e9311ad0845e7a06b82cd3f005b"
     end
   end
 

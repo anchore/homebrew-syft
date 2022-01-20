7def8b19d1e510eb363a0564a1fb4e6355e14bee
Alex Goodman <alex.goodman@anchore.com>
2022-01-20 12:00:38 -0500

    patch v0.36.0
    Signed-off-by: Alex Goodman <alex.goodman@anchore.com>

--- syft.rb
+++ syft.rb
@@ -11,7 +11,7 @@ class Syft < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/anchore/syft/releases/download/v0.36.0/syft_0.36.0_darwin_x86_64.tar.gz"
+      url "https://github.com/anchore/syft/releases/download/v0.36.0/syft_0.36.0_darwin_amd64.tar.gz"
       sha256 "fba022c6fac6f2d2f648295af78f86e873488565e41a252a97efafe75622ccf6"
     end
     if Hardware::CPU.arm?
@@ -22,7 +22,7 @@ class Syft < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/anchore/syft/releases/download/v0.36.0/syft_0.36.0_linux_x86_64.tar.gz"
+      url "https://github.com/anchore/syft/releases/download/v0.36.0/syft_0.36.0_linux_amd64.tar.gz"
       sha256 "5a69df410597d8649071b3419c17829f60d9f6f00edc8856b681842c2151f83c"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
